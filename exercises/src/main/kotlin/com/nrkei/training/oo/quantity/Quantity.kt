3d8062272865833c6ab77bd6918bda8fbd5f8238
Fred George <fredgeorge@acm.org>
2021-09-30 12:49:15 +0200

    Fred: Volume with extension methods

--- exercises/src/main/kotlin/com/nrkei/training/oo/quantity/Quantity.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/quantity/Quantity.kt
@@ -1,11 +1,13 @@
 package com.nrkei.training.oo.quantity
 
-class Quantity(amount: Number, private val unit: Unit) {
+class Quantity internal constructor(amount: Number, private val unit: Unit) {
     private val amount = amount.toDouble()
 
     override fun equals(other: Any?) = this === other || other is Quantity && this.equals(other)
 
-    private fun equals(other: Quantity) = this.amount == other.amount && this.unit == other.unit
+    private fun equals(other: Quantity) = this.amount == convertedAmount(other)
 
-    override fun hashCode() = amount.hashCode() * 37 + unit.hashCode()
+    private fun convertedAmount(other: Quantity) = this.unit.convertedAmount(other.amount, other.unit)
+
+    override fun hashCode() = unit.hashCode(amount)
 }
