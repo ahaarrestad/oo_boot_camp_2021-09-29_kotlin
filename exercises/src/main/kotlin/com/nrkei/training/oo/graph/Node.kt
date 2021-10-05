8297e7227b50f8ebe730a4c8d84f4b4176799261
Fred George <fredgeorge@acm.org>
2021-10-05 11:30:11 +0200

    Fred: Graph with Link with cost; cost not used yet

--- exercises/src/main/kotlin/com/nrkei/training/oo/graph/Node.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/graph/Node.kt
@@ -14,8 +14,6 @@ class Node {
         private const val UNREACHABLE = Double.POSITIVE_INFINITY
     }
 
-    infix fun to(neighbor: Node) = neighbor.also { links.add(Link(neighbor)) }
-
     infix fun canReach(destination: Node) = hopCount(destination, noVisitedNodes) != UNREACHABLE
 
     infix fun hopCount(destination: Node) = hopCount(destination, noVisitedNodes).also {
@@ -29,4 +27,10 @@ class Node {
     }
 
     private val noVisitedNodes = emptyList<Node>()
+
+    infix fun cost(amount: Number) = LinkBuilder(amount.toDouble(), links)
+
+    class LinkBuilder internal constructor(private val cost: Double, private val links: MutableList<Link>) {
+        infix fun to(neighbor: Node) = neighbor.also { links.add(Link(cost, neighbor))}
+    }
 }
