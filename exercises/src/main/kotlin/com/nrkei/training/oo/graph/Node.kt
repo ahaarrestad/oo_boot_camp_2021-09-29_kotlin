fa3e0bf27cae0f824a20a4fe692a04ffe0d03f08
Fred George <fredgeorge@acm.org>
2021-10-05 10:29:17 +0200

    Fred: Graph with hopCount(); not necessarily shortest; refactored

--- exercises/src/main/kotlin/com/nrkei/training/oo/graph/Node.kt
+++ exercises/src/main/kotlin/com/nrkei/training/oo/graph/Node.kt
@@ -12,7 +12,7 @@ class Node {
 
     infix fun to(neighbor: Node) = neighbor.also { neighbors.add(neighbor) }
 
-    infix fun canReach(destination: Node) = canReach(destination, noVisitedNodes)
+    infix fun canReach(destination: Node) = hopCount(destination, noVisitedNodes) != UNREACHABLE
 
     infix fun hopCount(destination: Node) = hopCount(destination, noVisitedNodes).also {
         require(it != UNREACHABLE) { "Destination is unreachable" }
@@ -26,13 +26,6 @@ class Node {
             it.hopCount(destination, visitedNodes).also { if (it != UNREACHABLE) return it + 1 }
         }
         return UNREACHABLE
-    }
-
-    private fun canReach(destination: Node, visitedNodes: MutableList<Node>): Boolean {
-        if (this == destination) return true
-        if (this in visitedNodes) return false
-        visitedNodes.add(this)
-        return neighbors.any { it.canReach(destination, visitedNodes) }
     }
 
     private val noVisitedNodes get() = mutableListOf<Node>()
